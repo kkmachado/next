89c09f8428b366539973c254192bcf9046d8969e
kkmachado <kkmachado@gmail.com>
2020-11-30 17:54:38 -0300

    Teste de preview

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import Card from '../components/card'
 function AboutPage() {
     return (
         <>
-            <Card title="Carlos Machado" text="Designer de produtos" />
+            <Card title="Carlos Machado" text="Designer de produtos e interfaces digitais" />
             <Link href="/" ><a>Voltar</a></Link>
         </>
     )
